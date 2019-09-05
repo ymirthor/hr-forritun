18acd2c88d1cc1ad8f885feb0bfb5706f3d45145
ymirthor <ymirthorleifsson@gmail.com>
2019-09-05 14:29:52 +0000

    first commit

---
+++ Verkefni 5/max_int.py
@@ -0,0 +1,8 @@
+ints_list=[]
+num_int=1
+while num_int>=0:
+     num_int = int(input("Input a number: "))    # Do not change this line
+     ints_list.append(num_int)
+
+max_int=max(ints_list)
+print("The maximum is", max_int)    # Do not change this line

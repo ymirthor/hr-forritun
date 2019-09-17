d54a5e080098398c89cd43a7f06e9fca0c156e3b
ymsi <ymirthorleifsson@gmail.com>
2019-09-17 08:50:58 +0000

    something added

--- PC/turtle_test.py
+++ PC/turtle_test.py
@@ -8,26 +8,26 @@ a.screensize()
 a.setup(width = 1.0, height = 1.0)
 
 
-tim=turtle.Turtle()
-tim.color('white')
-tim.pensize(0)
-tim.hideturtle()
-tim.speed(100)
+dreamy=turtle.Turtle()
+dreamy.color('white')
+dreamy.pensize(0)
+dreamy.hideturtle()
+dreamy.speed(100)
 
 def random_star():
     size=randint(1,15)    
-    tim.begin_fill()
+    dreamy.begin_fill()
     for x in range(5):
         
-        tim.forward(size)
-        tim.right(144)
-    tim.end_fill()
+        dreamy.forward(size)
+        dreamy.right(144)
+    dreamy.end_fill()
 def random_position():
-    tim.penup()
+    dreamy.penup()
     y=randint(-540,540)
     x=randint(-960,960)
-    tim.goto(x,y)
-    tim.pendown()
+    dreamy.goto(x,y)
+    dreamy.pendown()
 
 for x in range(500):
     random_star()
